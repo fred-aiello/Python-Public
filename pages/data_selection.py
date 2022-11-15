9674bf5b02267e5c07479aa16a20c8eded0d271b
fred-aiello <58234310+fred-aiello@users.noreply.github.com>
2022-11-15 20:19:26 +0100

    Update data_selection.py

--- pages/data_selection.py
+++ pages/data_selection.py
@@ -16,12 +16,12 @@ start_date=dt.date.today()
 col1, col2,col3, col4, col5,col6,col7,col8=st.columns(8)
 
 with col1:
-  if st.button('1D'):
+    st.button('1D')
     st.write(start_date)
 
 with col2:
-  if st.button('5D'):
-    st.write(start_date+timedelta(days=-5))
+   st.button('5D')
+   st.write(start_date+timedelta(days=-5))
 
 with col3:
   if st.button('1M'):
