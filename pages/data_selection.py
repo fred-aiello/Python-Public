be691fe185de010c3a0f2b4cbd9bce8d4db8e7f5
fred-aiello <58234310+fred-aiello@users.noreply.github.com>
2022-11-15 19:26:13 +0100

    Update data_selection.py

--- pages/data_selection.py
+++ pages/data_selection.py
@@ -12,5 +12,44 @@ st.write('You selected ',select_eq)
 
 start_date=dt.date.today()
 
+
+col1, col2,col3, col4, col5,col6,col7,col8=st.columns(8)
+
+with col1:
+if st.button('1D'):
+st.write(start_date)
+
+with col2:
+if st.button('5D'):
+st.write(start_date+timedelta(days=-5))
+
+with col3:
+if st.button('1M'):
+st.write(start_date+timedelta(months=-1))
+
+with col4:
+if st.button('6M'):
+st.write(start_date+timedelta(months=-6))
+
+with col5:
+if st.button('YTD'):
+st.write(start_date)
+
+with col6:
+if st.button('1Y'):
+st.write(start_date)
+
+with col7:
+if st.button('5Y'):
+st.write(start_date)
+
+with col8:
+if st.button('Max'):
+st.write(start_date)
+
+
+
+
 def delta_time(y,m,d):
+
 timedelta(
