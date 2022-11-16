8a492318991417cf5528f5737a25a588d751c24e
fred-aiello <58234310+fred-aiello@users.noreply.github.com>
2022-11-16 18:27:50 +0100

    Update page_4.py

--- pages/page_4.py
+++ pages/page_4.py
@@ -1,5 +1,15 @@
 # Contents of ~/my_app/pages/page_3.py
 import streamlit as st
 
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+    menu_items={
+    'Get Help':'https://www.yahoo.fr',
+   'Report a bug': "Https://www.boursotama.com'
+}
+)
+
+
 st.markdown("# Page 4 🎉")
 st.sidebar.markdown("# Page 4 🎉")
