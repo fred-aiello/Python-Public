f9787ef0d28ce3415f4bf55020159ffe685033bf
fred-aiello <58234310+fred-aiello@users.noreply.github.com>
2022-11-18 18:55:44 +0100

    Update data_selection.py

--- pages/data_selection.py
+++ pages/data_selection.py
@@ -33,7 +33,7 @@ st.dataframe(price_data)
 
 col1, col2,col3, col4, col5,col6,col7,col8=st.columns(8)
 st.dataframe(price_data.loc[price_data['Date']==start_date])
-st.write(price_data['Date'].max()]))
+st.write(price_data['Date'].max())
 
 
 with col1:
