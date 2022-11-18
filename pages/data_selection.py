bbc592da1ff5e7c75e811ce3f68b038b0dbe2aee
fred-aiello <58234310+fred-aiello@users.noreply.github.com>
2022-11-18 13:56:06 +0100

    Update data_selection.py

--- pages/data_selection.py
+++ pages/data_selection.py
@@ -31,7 +31,7 @@ col1, col2,col3, col4, col5,col6,col7,col8=st.columns(8)
 
 with col1:
     if st.button('1D'):
-      st.dataframe(price_data.loc[price_data['Date']<=start_date])
+      st.dataframe(price_data.loc[price_data['Date']>=start_date])
 
 with col2:
    if st.button('5D'):
