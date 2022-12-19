a9be4df37c4c2a78de480e4c1a149b8011829d07
fred-aiello <58234310+fred-aiello@users.noreply.github.com>
2022-12-19 14:52:16 +0100

    Update page_5.py

--- pages/page_5.py
+++ pages/page_5.py
@@ -8,7 +8,24 @@ from pdf2image import convert_from_path
 import PyPDF2
 import streamlit as st
 
+@st.cache
+def PDF_Reader(uploaded_file,FName):
+    
+    images = convert_from_path(uploaded_file)
+    pdf_writer = PyPDF2.PdfFileWriter()
+    
+    ## Convert a screened pdf '2019_2020_Reg.pdf' into a searchable pdf called 'searchable.pdf'
+    for image in images:
+        page = pytesseract.image_to_pdf_or_hocr(image, extension='pdf')
+        pdf = PyPDF2.PdfFileReader(io.BytesIO(page))
+        pdf_writer.addPage(pdf.getPage(0))
+    
+    # export the searchable PDF to searchable.pdf
+    with open(FName & "_search.pdf", "wb") as f:
+        pdf_writer.write(f)
+  
 
+    return st.markdown("Done")
 
 st.set_page_config(
     page_title="Hello Fred",
@@ -25,9 +42,8 @@ if uploaded_file is not None:
         
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
-    
-    st.write(uploaded_file.name)
+    #st.write(bytes_data)
+    Reader(uploaded_file,Fuploaded_file.name)
     
 
     '''
@@ -42,22 +58,7 @@ if uploaded_file is not None:
     # Can be used wherever a "file-like" object is accepted:
     dataframe = pd.read_csv(uploaded_file)
     st.write(dataframe)
-
-       
-    ## images = convert_from_path(uploaded_file)
-    pdf_writer = PyPDF2.PdfFileWriter()
-    
-    ## Convert a screened pdf '2019_2020_Reg.pdf' into a searchable pdf called 'searchable.pdf'
-    #for image in images:
-    #    page = pytesseract.image_to_pdf_or_hocr(image, extension='pdf')
-    #    pdf = PyPDF2.PdfFileReader(io.BytesIO(page))
-    #    pdf_writer.addPage(pdf.getPage(0))# export the searchable PDF to searchable.pdf
-    #with open("searchable.pdf", "wb") as f:
-    #    pdf_writer.write(f)
-
     '''
-
-    st.markdown("Done")
 
 st.markdown("# Page 5 🎉")
 st.sidebar.markdown("# Test OCR")
